4e3e6ddcda8f9d99d3d37d374b63d9cc2dc45075
damiana <d_antczak@op.pl>
2017-11-14 19:59:25 +0100

    Added PointUtils with point distance method

--- src/com/company/PointUtils.java
+++ src/com/company/PointUtils.java
@@ -5,6 +5,6 @@ public class PointUtils {
     public static double getDistance(Point point1, Point point2){
         double deltaX = point2.getX() - point1.getX();
         double deltaY = point2.getY() - point1.getY();
-        return Math.sqrt(deltaX + deltaY);
+        return Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2));
     }
 }
