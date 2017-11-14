6e5c68cd67532c16421f64e82e3d3e52c4a4faf3
Czupryniak, Michal <michal.czupryniak@pearson.com>
2017-11-14 19:45:27 +0100

    NearestNeighbor class

---
+++ src/com/company/NearestNeighbor.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Map;
+
+public class NearestNeighbor {
+    public static void execute(Point startPoint, Map<Integer, Point> allPoints) {
+
+    }
+
+    private static void findNearest(Point point) {
+
+    }
+
+    private static void countProfit(Integer profit, Integer wayLoss) {
+
+    }
+}
