8385be65834a49b2964e6a8e60a55a7d8a0bc5d3
Czupryniak, Michal <michal.czupryniak@pearson.com>
2017-11-19 00:42:43 +0100

    NearestNeighbor

--- src/com/company/NearestNeighbor.java
+++ src/com/company/NearestNeighbor.java
@@ -3,15 +3,35 @@ package com.company;
 import java.util.Map;
 
 public class NearestNeighbor {
-    public static void execute(Point startPoint, Map<Integer, Point> allPoints) {
+    private static Map<Integer, Point> tempPoints;
+    public static void execute(Integer startPointIndex, Map<Integer, Point> allPoints) {
+        Point latestPoint = allPoints.get(startPointIndex);
+        tempPoints.put(0, latestPoint);
+        allPoints.remove(startPointIndex);
 
+        allPoints.forEach((integer, point) -> {
+            NearestNeighbor.getDistance(latestPoint, point);
+        });
     }
 
-    private static void findNearest(Point point) {
+    private static Integer findNearest(Point point, Map<Integer, Point> allPoints) {
+        Integer min = 10000;
+        allPoints.forEach((integer, secondPoint) -> {
+            if(NearestNeighbor.getDistance(point, secondPoint) < min) {
+                min = NearestNeighbor.getDistance(point, secondPoint);
+            }
+        });
 
+        return min;
     }
 
-    private static void countProfit(Integer profit, Integer wayLoss) {
+    private static Integer countProfit(Integer profit, Integer wayLoss) {
+        Integer LOSS_CONST = 5;
 
+        return profit - wayLoss * LOSS_CONST;
+    }
+
+    private static double getDistance(Point pointOne, Point pointTwo) {
+        return Math.sqrt((pointOne.getX() - pointTwo.getX()) *  (pointOne.getX() - pointTwo.getX()) + (pointOne.getY() - pointTwo.getY()) *  (pointOne.getY() - pointTwo.getY()));
     }
 }
