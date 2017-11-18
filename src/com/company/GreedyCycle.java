add374de5dbc5ce4082cc4a9afa4852de4bec81c
damiana <d_antczak@op.pl>
2017-11-18 17:52:46 +0100

    Added save point to bmp file

--- src/com/company/GreedyCycle.java
+++ src/com/company/GreedyCycle.java
@@ -2,51 +2,75 @@ package com.company;
 
 import javafx.util.Pair;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class GreedyCycle implements Algorithm{
+public class GreedyCycle implements Algorithm {
 
-    private List<Point> cycle;
+    private LinkedList<Point> cycle;
     private Point startPoint;
 
     public GreedyCycle() {
-        this.cycle = new ArrayList<>();
+        this.cycle = new LinkedList<>();
     }
 
     @Override
     public void execute(Point startPoint, Map<Integer, Point> allPoints) {
         this.startPoint = startPoint;
-        allPoints.remove(startPoint);
+        allPoints.remove(startPoint.getIndex());
+        cycle.add(startPoint);
 
-        findStartCycle(allPoints);
+        findPath(allPoints);
+
+        cycle.forEach(point -> System.out.println(point.getIndex()));
     }
 
-    private void findStartCycle(Map<Integer, Point> points){
+    private Pair<Point, Double> findNearestPoint(Point forPoint,Map<Integer, Point> points) {
         double distance = 0.0;
         Point currentPoint = null;
 
         Iterator it = points.entrySet().iterator();
         while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
+            Map.Entry pair = (Map.Entry) it.next();
             Point point = (Point) pair.getValue();
             Integer key = (Integer) pair.getKey();
-            double distanceToStart = startPoint.getDistance(point);
+            double distanceToStart = forPoint.getDistance(point);
 
-            System.out.println(key + " = " + distanceToStart);
+//            System.out.println(key + " = " + distanceToStart);
 
-            if(distance == 0.0 || distance > distanceToStart) {
+            if (distance == 0.0 || distance > distanceToStart) {
                 distance = distanceToStart;
                 currentPoint = point;
             }
-
-            it.remove();
         }
 
-        System.out.println(distance);
-        System.out.println(currentPoint.getIndex());
-        cycle.add(currentPoint);
+//        System.out.println(distance);
+//        System.out.println(currentPoint.getIndex());
+
+        return new Pair<>(currentPoint, distance);
+    }
+
+    private Map<Integer, Point> expandCycle(Map<Integer, Point> points) {
+        Point currentPoint = null;
+        Point precursor = null;
+        double distance = 0;
+        for (Point point : cycle) {
+            Pair<Point, Double> nearestPoint = findNearestPoint(point, points);
+            if (distance == 0 || distance > nearestPoint.getValue()) {
+                distance = nearestPoint.getValue();
+                currentPoint = nearestPoint.getKey();
+                precursor = point;
+            }
+        }
+
+
+        cycle.add(cycle.indexOf(precursor),currentPoint);
+        points.remove(currentPoint.getIndex());
+        return points;
+    }
+
+    private void  findPath(Map<Integer, Point> points){
+        while (points.size() > 0){
+            points = expandCycle(points);
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,12 @@
 package com.company;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 
 public class Main {
@@ -15,10 +21,41 @@ public class Main {
 
             Map<Integer, Point> points = pointsReader.getPoints();
 
+            int maxX = Collections.max(points.entrySet(), Comparator.comparingInt(entry -> entry.getValue().getX())).getValue().getX();
+            int maxY = Collections.max(points.entrySet(), Comparator.comparingInt(entry -> entry.getValue().getY())).getValue().getY();
+            BufferedImage img = new BufferedImage(maxX + 50, maxY + 50, BufferedImage.TYPE_INT_RGB);
+
+            int pixelWhite[] = {255,255,255};
+            for (int i = 0 ; i < img.getWidth(); i++){
+                for (int j = 0 ; j < img.getHeight(); j++){
+                    img.getRaster().setPixel(i,j,pixelWhite);
+                }
+            }
+
+            points.forEach((integer, point) -> {
+                int pixel[] = {0,0,0};
+                for (int size = 1 ; size < 12; size++) {
+                    img.getRaster().setPixel(point.getX() - size, point.getY() - size, pixel);
+                    img.getRaster().setPixel(point.getX() + size, point.getY() + size, pixel);
+                    img.getRaster().setPixel(point.getX() - size, point.getY() + size, pixel);
+                    img.getRaster().setPixel(point.getX() + size, point.getY() - size, pixel);
+                }
+                img.getRaster().setPixel(point.getX(), point.getY(), pixel);
+            });
+
+            ImageIO.write(img, "BMP", new File("points.bmp"));
+
             GreedyCycle greedyCycle = new GreedyCycle();
             greedyCycle.execute(points.get(1), points);
+
+
+
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
