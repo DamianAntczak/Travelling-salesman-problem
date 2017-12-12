ef0b18dc05dd163e2d1bdda39a18d4d2714681c5
Czupryniak, Michal <michal.czupryniak@pearson.com>
2017-12-12 19:11:41 +0100

    Commons percentage functions added

--- src/com/company/MeasuresOfSimilarity.java
+++ src/com/company/MeasuresOfSimilarity.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class MeasuresOfSimilarity {
 
-    public static final int INITIAL_CAPACITY = 100;
+    public static final int INITIAL_CAPACITY = 10;
 
     public void execute(Map<Integer, Point> points){
         List<Algorithm> algorithms = new ArrayList<>(INITIAL_CAPACITY);
@@ -17,6 +17,33 @@ public class MeasuresOfSimilarity {
             algorithms.add(randomPath);
             System.out.println("Add i: "+ i);
         }
+    }
 
+    public double commonVertexs(Algorithm solution1, Algorithm solution2) {
+        final int[] howManyCommon = {0};
+        solution1.getResultList().forEach((point) -> {
+            if (solution2.getResultList().indexOf(point) != -1) {
+                howManyCommon[0] += 1;
+            }
+        });
+
+        double avgVertexsCount = (solution1.getResultList().size() + solution2.getResultList().size()) / 2;
+        return howManyCommon[0] / avgVertexsCount;
+    }
+
+    public double commonEdges(Algorithm solution1, Algorithm solution2) {
+        int howManyCommon = 0;
+        for(int index1 = 0; index1 < solution1.getResultList().size()-1; index1++) {
+            for(int index2 = 0; index2 < solution2.getResultList().size()-1; index2++) {
+                if(solution1.getResultList().get(index1).getIndex() == solution2.getResultList().get(index2).getIndex() &&
+                        solution1.getResultList().get(index1+1).getIndex() == solution2.getResultList().get(index2+1).getIndex()) {
+                    howManyCommon += 1;
+                    break;
+                }
+            }
+        }
+
+        double avgEdgesCount = (solution1.getResultList().size() + solution2.getResultList().size()) / 2;
+        return howManyCommon / avgEdgesCount;
     }
 }
